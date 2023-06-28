c7c4de00d1498e7baed6cf2a07c58f49419532a8
Juan Carlos <juancarlosvelasquez619@gmail.com>
2023-06-28 16:40:09 -0500

    update

--- js/main.js
+++ js/main.js
@@ -184,3 +184,47 @@ const productos = [
         precio: 1000
     }
 ];
+
+const contenedorProductos = document.querySelector("#contenedor-productos");
+const botonesCategorias = document.querySelector(".boton-categoria");
+
+function cargarProductos(productosElegidos) {
+
+    contenedorProductos.innerHTML = "";
+
+    productosElegidos.forEach(producto => {
+
+        const div = document.createElement("div");
+        div.classList.add("producto");
+        div.innerHTML = `
+            <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+            <div class="producto-detalles">
+          <h3 class="producto-titulo">${producto.titulo}</h3>
+          <p class="producto-precio">$${producto.precio}</p>
+          <button class="producto-agregar" id="${producto.id}">Agregar</button>
+        </div>
+        `;
+
+        contenedorProductos.append(div);
+    })
+}
+
+cargarProductos(productos);
+
+botonesCategorias.forEach(boton => {
+   boton.addEvenListener("click", (e) => {
+
+        botonesCategorias.forEach(boton => boton.classList.revome("active"));
+        e.currentTarget.classList.add("active");
+
+        if(e.currentTarget.id != "todos") {
+            const productosBoton = producto.filter(producto => producto.categoria.id === e.currentTarget.id);
+            cargarProductos(productosBoton);
+        } else {
+            cargarProductos(productos);
+        }
+
+    })
+})
+
+
